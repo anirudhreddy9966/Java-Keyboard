40664f07608e6a39b4e2c8e379a7d68d008a1103
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 01:22:33 -0500

    Add files via upload

---
+++ Keyboard.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class Keyboard
+{
+  public static void main(String args[])
+  {
+   
+   Scanner sc=new Scanner(System.in);
+   int a;
+   a=sc.nextInt();
+   System.out.println("a is "+ a);
+  }
+}
